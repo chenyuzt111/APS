3e79b520ebd093258cd8aa2839f93795705131a1
itcast <1045112154@qq.com>
2023-11-27 17:02:28 +0800

    11.27主板固定表

---
+++ src/main/java/com/benewake/system/service/scheduling/mes/ApsTfminiSFixedService.java
@@ -0,0 +1,15 @@
+package com.benewake.system.service.scheduling.mes;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.benewake.system.entity.ApsTfminiSFixed;
+import com.benewake.system.service.ApsIntfaceDataServiceBase;
+
+
+/**
+* @author DELL
+* @description 针对表【aps_tfmini_s_fixed】的数据库操作Service
+* @createDate 2023-11-27 14:27:02
+*/
+public interface ApsTfminiSFixedService extends IService<ApsTfminiSFixed> , ApsIntfaceDataServiceBase {
+
+}
